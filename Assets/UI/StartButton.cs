b19dc435e44b1ba0ea30d30395aef29316efbb05
laurelthrone <laurelthrone@gmail.com>
2020-08-08 12:45:08 -0700

    work on title screen (somehow broke level transitions??)

---
+++ Assets/UI/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+
+    public Button button;
+    public Scener scener;
+
+    // Update is called once per frame
+    void Start()
+    {
+        button.onClick.AddListener(StartGame);
+    }
+        
+    void StartGame()
+    {
+        scener.GoToScene("1");
+    }
+}
