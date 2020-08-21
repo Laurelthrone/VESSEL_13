a0f820c2cab9add49d2b4083d3d7c6457f030c01
Laurelthrone <laurelthrone@gmail.com>
2020-08-20 21:14:32 -0700

    removed random unused variable that was just called a???????

--- Assets/Scripts/Environment/BeastGroundDetector.cs
+++ Assets/Scripts/Environment/BeastGroundDetector.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BeastGroundDetector : MonoBehaviour
 {
-    int a = 0;
     void OnTriggerExit2D(Collider2D collider)
     {
         if (collider.tag == "Ground") SendMessageUpwards("Flip"); 
