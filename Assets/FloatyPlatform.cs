461fe0a4efd606ac0759553852c7214430bdfcbc
Laurelthrone <laurelthrone@gmail.com>
2020-08-23 14:59:06 -0700

    66

---
+++ Assets/FloatyPlatform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatyPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(8, 16, true);
+    }
+}
